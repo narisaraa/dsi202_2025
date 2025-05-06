03271bb02cadfb44128e74f0de260341e131900a
narisaraa <narisara.konw@gmail.com>
2025-05-06 17:35:18 +0700

    restart project

--- adoption/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import home  # ← เชื่อมกับ views.py ที่แก้ไขแล้ว!
-
-urlpatterns = [
-    path('', home, name='home'),
-]
--- adoption/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'adoption/home.html')  # ← ให้ Django แสดงหน้า home.html
